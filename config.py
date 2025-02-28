66b93c175d9f309356210030c6ab73941ace93da
Dvis <gokhaledurgesh4@gmail.com>
2025-02-28 22:49:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,50 +1,18 @@
 import os
-import logging
 from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
 
 ADMINS = list(map(int, os.environ.get('ADMINS', '1679112664').split()))
-if not ADMINS:
-    logging.error("ADMINS variable is missing! Exiting now")
-    exit(1)
-
 api_id = os.environ.get('TELEGRAM_API', '')
-if not api_id:
-    logging.error("TELEGRAM_API variable is missing! Exiting now")
-    exit(1)
-
 api_hash = os.environ.get('TELEGRAM_HASH', '')
-if not api_hash:
-    logging.error("TELEGRAM_HASH variable is missing! Exiting now")
-    exit(1)
-
 bot_token = os.environ.get('BOT_TOKEN', '')
-if not bot_token:
-    logging.error("BOT_TOKEN variable is missing! Exiting now")
-    exit(1)
-
 dump_id = os.environ.get('DUMP_CHAT_ID', '')
-if not dump_id:
-    logging.error("DUMP_CHAT_ID variable is missing! Exiting now")
-    exit(1)
-else:
-    dump_id = int(dump_id)
-
 fsub_id = os.environ.get('FSUB_ID', '')
-if not fsub_id:
-    logging.error("FSUB_ID variable is missing! Exiting now")
-    exit(1)
-else:
-    fsub_id = int(fsub_id)
-
 mongo_url = os.environ.get('MONGO_URL', '')
-if not mongo_url:
-    logging.error("MONGO_URL variable is missing! Exiting now")
-    exit(1)
+
 
 
 #Database 
@@ -56,8 +24,52 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 # check my discription to help by using my refer link of shareus.io
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "ziplinker.net")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "4b884da539a2f5d579e2a6f805e623c7a082a3b1")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", None)
+SHORTLINK_API = os.environ.get("SHORTLINK_API", None)
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
+
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+if not ADMINS:
+    logging.error("ADMINS variable is missing! Exiting now")
+    exit(1)
+
+if not api_id:
+    logging.error("TELEGRAM_API variable is missing! Exiting now")
+    exit(1)
+
+if not api_hash:
+    logging.error("TELEGRAM_HASH variable is missing! Exiting now")
+    exit(1)
+
+if not bot_token:
+    logging.error("BOT_TOKEN variable is missing! Exiting now")
+    exit(1)
+
+if not dump_id:
+    logging.error("DUMP_CHAT_ID variable is missing! Exiting now")
+    exit(1)
+else:
+    dump_id = int(dump_id)
+
+if not fsub_id:
+    logging.error("FSUB_ID variable is missing! Exiting now")
+    exit(1)
+else:
+    fsub_id = int(fsub_id)
+
+if not mongo_url:
+    logging.error("MONGO_URL variable is missing! Exiting now")
+    exit(1)
+
+
+
+
+
+
+
