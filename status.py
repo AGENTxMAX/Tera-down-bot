3cd143771935c2efbf6dcf4f76195afeb51d0846
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-03-01 10:55:29 +0530

    Update status.py

--- status.py
+++ status.py
@@ -1,4 +1,4 @@
-def format_progress_bar(filename, percentage, done, total_size, status, eta, speed, elapsed, user_mention, user_id, aria2p_gid):
+def format_progress_bar(filename, percentage, done, total_size, status, eta, speed, elapsed, user_mention, user_id):
     bar_length = 10
     filled_length = int(bar_length * percentage / 100)
     bar = '★' * filled_length + '☆' * (bar_length - filled_length)
