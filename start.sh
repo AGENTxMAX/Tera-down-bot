cdf5eaa825513a818b0055126a1cbbce17093e22
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-03-20 23:23:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-python3 terabox.py
+#!/bin/sh
+
+# Ensure moviepy installs correctly
+pip install --no-cache-dir --force-reinstall -r requirements.txt
+
+# Start aria2c in the background
+aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon &
+
+# Run your Python script
+python terabox.py
